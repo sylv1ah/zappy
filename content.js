2b712d49fc849c3ae1551e422728360f048a55a6
Joko Sanyang <jo.san@live.co.uk>
2019-07-12 15:46:19 +0100

    Add function to add cost per wear to ASOS pages under price

--- content.js
+++ content.js
@@ -3,3 +3,15 @@ console.log(price[0].innerText);
 
 let material = document.getElementsByClassName('about-me');
 console.log(material[0].innerText);
+
+const calculateCostPerWear = price => {
+    let lifetimeUse = 2;
+    const costPerWear = price / lifetimeUse;
+    return costPerWear.toFixed(2);
+}
+
+const itemCost = parseInt(price[0].innerText.replace(/£/,""));
+
+const displayCostPW = document.createElement('p');
+displayCostPW.innerText = `Cost per wear: £${calculateCostPerWear(itemCost)}`;
+price[0].appendChild(displayCostPW);
