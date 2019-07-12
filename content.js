ecbb94628cced037ad88172c56834c7021f0557e
Joko Sanyang <jo.san@live.co.uk>
2019-07-12 15:45:06 +0100

    Add items to match array to manifest

--- content.js
+++ content.js
@@ -1,17 +1,5 @@
-// let imageNodes = document.getElementsByClassName('product-image');
-// let pageArray = [...imageNodes];
-// let images = pageArray.filter(item => {
-//     return item.nodeName == "IMG";
-// })
-// console.log(images[0].src);
-
 let price = document.getElementsByClassName('current-price');
 console.log(price[0].innerText);
 
 let material = document.getElementsByClassName('about-me');
 console.log(material[0].innerText);
-
-let testing = document.createElement('p');
-testing.innerText = "Will this work?";
-material[0].appendChild(testing);
-
