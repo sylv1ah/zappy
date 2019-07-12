d6434873f5c41023be50e9f186f9f58cef827572
seabasshoang <sylviahoang@hotmail.co.uk>
2019-07-12 17:39:46 +0100

    create top bar with close button

--- content.js
+++ content.js
@@ -1,23 +1,38 @@
-let price = document.getElementsByClassName('current-price');
+let price = document.getElementsByClassName("current-price");
 console.log(price[0].innerText);
 
-let material = document.getElementsByClassName('about-me');
+let material = document.getElementsByClassName("about-me");
 console.log(material[0].innerText);
 
 const calculateCostPerWear = price => {
-    let lifetimeUse = 2;
-    const costPerWear = price / lifetimeUse;
-    return costPerWear.toFixed(2);
-}
+  let lifetimeUse = 2;
+  const costPerWear = price / lifetimeUse;
+  return costPerWear.toFixed(2);
+};
 
-const itemCost = parseInt(price[0].innerText.replace(/£/,""));
+const itemCost = parseInt(price[0].innerText.replace(/£/, ""));
 
-const displayCostPW = document.createElement('p');
+const displayCostPW = document.createElement("p");
 displayCostPW.innerText = `Cost per wear: £${calculateCostPerWear(itemCost)}`;
 price[0].appendChild(displayCostPW);
 
-let header = document.createElement('header');
-header.classList.add('sticky');
-document.body.appendChild(header);
+let header = document.createElement("div");
+header.classList.add("sticky");
+document.body.parentNode.insertBefore(header, document.body.nextSibling);
+document.body.classList.add("newBody");
 
-console.log('This should be the header:', document.body.header);
+let headerItem = document.createElement("div");
+headerItem.innerText = `Item cost: £${itemCost}, Cost per wear: £${calculateCostPerWear(
+  itemCost
+)}`;
+header.appendChild(headerItem);
+
+let closeButton = document.createElement("button");
+closeButton.innerText = "close";
+closeButton.addEventListener("click", () => {
+  header.remove();
+  document.body.classList.remove("newBody");
+});
+header.appendChild(closeButton);
+
+console.log("This should be the header:", document.body.header);
