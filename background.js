f49fb7a94727bf350354f04e667df0efe4f7395b
Freddie <freddiepreece@outlook.com>
2019-07-20 17:50:34 +0100

    Restrict pop-up to only work on certain sites

--- background.js
+++ background.js
@@ -1,13 +1,76 @@
+let items = ["anorak",
+  "apron",
+  "baseball-cap",
+  "belt",
+  "bikini",
+  "blazer",
+  "blouse",
+  "boots",
+  "bow-tie",
+  "boxers",
+  "bra",
+  "cardigan",
+  "coat",
+  "dress",
+  "dressing-gown",
+  "gloves",
+  "hat",
+  "high-heels",
+  "jacket",
+  "jeans",
+  "jumpsuit",
+  "jumper",
+  "knickers",
+  "leather-jacket",
+  "leggings",
+  "miniskirt",
+  "nightie",
+  "overalls",
+  "overcoat",
+  "pants",
+  "pyjamas",
+  "raincoat",
+  "sandals",
+  "scarf",
+  "shirt",
+  "shoelace",
+  "shoes",
+  "shorts",
+  "skirt",
+  "slippers",
+  "socks",
+  "stilettos",
+  "stockings",
+  "suit",
+  "sweater",
+  "sweatshirt",
+  "swimming-costume",
+  "swimming-trunks",
+  "thong",
+  "tie",
+  "tights",
+  "top",
+  "tracksuit",
+  "trainers",
+  "trousers",
+  "t-shirt",
+  "underpants",
+  "vest",
+  "wellingtons"];
+
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color: "#3aa757" }, () => {
-    console.log("The color is green.");
-  });
   chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
     chrome.declarativeContent.onPageChanged.addRules([
       {
-        conditions: [new chrome.declarativeContent.PageStateMatcher({})],
+        conditions: [new chrome.declarativeContent.PageStateMatcher(
+          {
+            pageUrl: {
+              urlContains:
+                items.forEach(item => item),
+            })],
         actions: [new chrome.declarativeContent.ShowPageAction()]
       }
     ]);
   });
 });
+
